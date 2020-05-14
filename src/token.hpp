69901ae7ef6ae4c3974b8c028be004e8af3b938d
neo <sjkyspa@gmail.com>
2020-05-14 16:12:05 +0800

    feat: should able to construct token

--- src/token.hpp
+++ src/token.hpp
@@ -8,14 +8,25 @@ public:
   union value {
     double d;
     std::string s;
+    value() {}
+    value(double v) : d(v) {}
+    value(std::string v) : s(v) {}
+    ~value() {}
   };
-
-  explicit token(type t, std::string  v) {
-    _t = t;
-  }
   
- private:
-  token::type _t;
+  explicit token(type t, std::string v): _t(t), _v{v} {
+    
+  }
+
+  token(const token & tok) {
+    _t = tok._t;
+    _v.s = {tok._v.s};
+  }
+    
+
+private:
+  type _t;
+  value _v;
 };
 
-#endif// TOKEN_HPP
+#endif // TOKEN_HPP
--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -1,25 +1,25 @@
+#include "tokenizer.hpp"
+#include "stream.hpp"
+#include "token.hpp"
 #include <iostream>
 #include <string>
-#include "tokenizer.hpp"
-#include "token.hpp"
-#include "stream.hpp"
 
-stream<token> tokenizer::tokenize(const std::istream & in) {
+stream<token> tokenizer::tokenize(const std::istream &in) {
 
   std::function<std::unique_ptr<item<token>>()> func;
   std::function<std::unique_ptr<item<token>>()> func1;
-  func1 = [&]{
-                                                         std::string t;
-                                                         std::cin >> t;
-                                                         return std::make_unique<item<token>>(token(token::type::def, t),
-                                                                                              stream<token>());
-                                                       };
-  func = [&]{
-                                                         std::string t;
-                                                         std::cin >> t;
-                                                         return std::make_unique<item<token>>(token(token::type::def, t),
-                                                                                              stream<token>(func1));
-                                                       };
-      return stream<token>(func);
+  func1 = [&] {
+    std::string t;
+    std::cin >> t;
+    return std::make_unique<item<token>>(token(token::type::def, t),
+                                         stream<token>());
+  };
   
-} 
+  func = [&] {
+    std::string t;
+    std::cin >> t;
+    return std::make_unique<item<token>>(token(token::type::def, t),
+                                         stream<token>(func1));
+  };
+  return stream<token>(func);
+}
